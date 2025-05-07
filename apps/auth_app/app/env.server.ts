5daa2fd054d3814254d8f4b24c3606e2ffac3cfa
Leonard Lawson <lleasy8@gmail.com>
2025-05-07 14:01:51 +0700

    expose entry server and init env vars

---
+++ apps/auth_app/app/env.server.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+const envSchema = z.object({
+ 
+});
+
+export const initEnvVariables = () => {
+  const env = envSchema.safeParse(process.env);
+  if (!env.success) {
+    console.error(
+      "Invalid environment variables",
+      env.error.flatten().fieldErrors
+    );
+    throw new Error("Invalid environment variables");
+  }
+  return env.data;
+};
+
+export const getServerEnv = () => initEnvVariables();
+
+// manunally add client env variables
+export const getClientEnv = () => {};
+
+
+export type CLIENT_ENV = ReturnType<typeof getClientEnv>;
+type APP_ENV = z.infer<typeof envSchema>;
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv extends APP_ENV {}
+  }
+}
