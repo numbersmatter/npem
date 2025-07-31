948605f25b33e3ad9ecff493a72ee3100ea0ffe9
MarkMutt <lleasy8@gmail.com>
2025-07-31 17:54:20 -0400

    bauth writes to addresses

--- apps/bauth/app/routes/address.tsx
+++ apps/bauth/app/routes/address.tsx
@@ -1,13 +1,26 @@
+import { db } from "~/services/db/db.server";
 import type { Route } from "./+types/address";
+import { addresses } from "~/services/db/schema";
 
 
 export async function action({ request }: Route.ActionArgs) {
   // Handle the form submission for address
   const formData = await request.formData();
   const address = formData.get("address") as string;
+  // Process the address data as needed
+  await db.insert(addresses).values({
+    id: "123445",
+    // firstName: formData.get("first-name") as string,
+    // lastName: formData.get("last-name") as string,
+    streetAddress: formData.get("street-address") as string,
+    secondaryAddress: formData.get("secondary-address") as string,
+    city: formData.get("city") as string,
+    state: formData.get("region") as string,
+    zipCode: formData.get("postal-code") as string,
+  });
 
   // Here you would typically save the address to a database or perform some action
-  console.log("Address submitted:", address);
+
 
   // Redirect or return a response as needed
   return { success: true, message: "Address saved successfully!" };
@@ -25,26 +38,128 @@ export default function AddressPage({ }: Route.ComponentProps) {
     </div>
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
       {/* <RegisterCard /> */}
+      <form method="POST"
+        className="bg-white shadow-xs ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2"
+      >
+        <div className="px-4 py-6 sm:p-8">
+          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
 
-      <div>
-        <label htmlFor="address" className="block text-sm/6 font-medium text-gray-900">
-          Address
-        </label>
-        <div className="mt-2">
-          <input
-            id="address"
-            name="address"
-            type="text"
-            placeholder="123 Main St, Anytown, USA"
-            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-          />
+            <div className="sm:col-span-3">
+              <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label htmlFor="street-address" className="block text-sm/6 font-medium text-gray-900">
+                Street address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="street-address"
+                  name="street-address"
+                  type="text"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div className="col-span-full">
+              <label htmlFor="secondary-address" className="block text-sm/6 font-medium text-gray-900">
+                Secondary address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="secondary-address"
+                  name="secondary-address"
+                  type="text"
+                  autoComplete="address-line2"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="city" className="block text-sm/6 font-medium text-gray-900">
+                City
+              </label>
+              <div className="mt-2">
+                <input
+                  id="city"
+                  name="city"
+                  type="text"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="region" className="block text-sm/6 font-medium text-gray-900">
+                State / Province
+              </label>
+              <div className="mt-2">
+                <input
+                  id="region"
+                  name="region"
+                  type="text"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="postal-code" className="block text-sm/6 font-medium text-gray-900">
+                ZIP / Postal code
+              </label>
+              <div className="mt-2">
+                <input
+                  id="postal-code"
+                  name="postal-code"
+                  type="text"
+                  autoComplete="postal-code"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="mt-6">
-        <button className="w-full rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-          Save Address
-        </button>
-      </div>
+        <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+          <button type="button" className="text-sm/6 font-semibold text-gray-900">
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+
     </div>
   </div>;
 
