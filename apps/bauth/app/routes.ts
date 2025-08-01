eeff6c35eac37a11b73839b663e8344271f921b3
MarkMutt <lleasy8@gmail.com>
2025-08-01 11:45:48 -0400

    bauth created profile layout pages

--- apps/bauth/app/routes.ts
+++ apps/bauth/app/routes.ts
@@ -1,10 +1,16 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
   route("api/auth/*", "routes/auth_api.tsx"),
   route("protected", "routes/protected.tsx"),
   route("login", "routes/login.tsx"),
+  layout("./routes/layout.tsx", [route("profile", "routes/profile.tsx")]),
   route("register", "routes/register.tsx"),
   route("address", "routes/address.tsx"),
 ] satisfies RouteConfig;
